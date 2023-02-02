b9faed149f7d2d5cea021a0032020cfca5d17912
geuber <geuber@supersimplescred.com>
2023-02-02 18:51:21 -0300

    pequenas alterações

--- projeto_para_estudos/Controllers/HomeController.cs
+++ projeto_para_estudos/Controllers/HomeController.cs
@@ -11,12 +11,7 @@ namespace projeto_para_estudos.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        
 
         public IActionResult Index()
         {
