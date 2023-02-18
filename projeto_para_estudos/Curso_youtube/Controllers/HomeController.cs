510b5e673f5e738fb10ebc780378e7a1ef01e1f5
geuber <geuber@supersimplescred.com>
2023-02-18 11:23:23 -0300

    PPRIMEIRO PROJETO MVC

---
+++ projeto_para_estudos/Curso_youtube/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Curso_youtube.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Curso_youtube.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+                home.Email = "geuberlu@gmai.com";
+            home.Name = "Geuber Lucas";
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ projeto_para_estudos/Curso_youtube/Models/HomeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Curso_youtube.Models
+{
+    public class HomeModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
